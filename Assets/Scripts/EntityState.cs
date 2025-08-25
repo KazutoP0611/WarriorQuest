a69e7a4742f33b69fcbb2bc24f08bdd815337206
PPYukito <PanutatR@gmail.com>
2025-08-25 09:29:22 +0900

    [Push] Push project repository;

---
+++ Assets/Scripts/EntityState.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public abstract class EntityState
+{
+    protected Player player;
+    protected StateMachine stateMachine;
+    protected string animBoolName;
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public EntityState(Player player, StateMachine stateMachine, string animBoolName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+
+        anim = player.anim;
+        rb = player.rb;
+    }
+
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+    }
+
+    public virtual void Update()
+    {
+        Debug.Log($"I am running up of {animBoolName}");
+    }
+
+    public virtual void Exit()
+    {
+        anim.SetBool(animBoolName, false);
+    }
+}
